7d07c4cea8e9f4e38b83544c84745c6db14adcd0
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-06 19:20:39 -0300

    feat: criando estrutura básica do modal de cadastro de autorização

---
+++ src/components/create-authorization-form.tsx
@@ -0,0 +1,17 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+export function CreateAuthorizationForm() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Cadasrar nova autorização</DialogTitle>
+        <DialogDescription>Conteúdo do modal</DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  );
+}
