217f121542b22acd37b53b925643e4820b855c03
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-06 19:18:05 -0300

    feat: criado função de logout para separar na sidebar

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,23 +1,27 @@
-'use server';
- 
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
- 
+"use server";
+
+import { signIn, signOut } from "@/auth";
+import { AuthError } from "next-auth";
+
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
 ) {
   try {
-    await signIn('credentials', formData);
+    await signIn("credentials", formData);
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
+        case "CredentialsSignin":
+          return "Invalid credentials.";
         default:
-          return 'Something went wrong.';
+          return "Something went wrong.";
       }
     }
     throw error;
   }
 }
+
+export const logout = async () => {
+  await signOut({ redirectTo: "/login" });
+};
