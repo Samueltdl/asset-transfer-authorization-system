bfd50dd0f85eb18233887baa0cddc1a8178e3d47
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-05 21:01:00 -0300

    feat: criando arqivo auth adaptado a partir da documentação

---
+++ src/auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "./auth.config";
+import { z } from "zod";
+import bcrypt from "bcryptjs";
+import prisma from "./lib/prisma";
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(5) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await prisma.user.findUnique({ where: { email } });
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          console.log("User found:", user.email);
+          if (passwordsMatch) {
+            return {
+              ...user,
+              id: String(user.id), // convertido o ID para string, pois o NextAuth espera que seja string
+            };
+          }
+        }
+
+        console.log("Invalid credentials");
+        return null;
+      },
+    }),
+  ],
+});
