3bc540ef0de6534f7d58f75fb714a19d77e7d9b9
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-06 21:35:15 -0300

    feat: adicionando try catch com tratamento de erros para o create-authorization

--- src/actions/create-authorization.ts
+++ src/actions/create-authorization.ts
@@ -9,18 +9,24 @@ export async function createAuthorization(data: AuthorizationFormValues) {
   const session = await auth();
   if (!session?.user?.id) throw new Error("Não autorizado");
 
-  await prisma.authorization.create({
-    data: {
-      origin: data.origin,
-      destination: data.destination,
-      responsible: data.responsible,
-      motive: data.motive,
-      userId: Number(session.user.id),
-      items: {
-        create: data.items,
+  try {
+    await prisma.authorization.create({
+      data: {
+        origin: data.origin,
+        destination: data.destination,
+        responsible: data.responsible,
+        motive: data.motive,
+        userId: Number(session.user.id), // Convertendo para number conforme seu schema
+        items: {
+          create: data.items,
+        },
       },
-    },
-  });
+    });
 
-  revalidatePath("/dashboard");
+    revalidatePath("/dashboard");
+    return { success: true };
+  } catch (error) {
+    console.error("Erro ao cadastrar:", error);
+    return { error: "Falha ao salvar a autorização no banco de dados." };
+  }
 }
