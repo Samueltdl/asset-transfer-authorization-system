acbbe4d534d74e87e9b9698d7cb7b39c6b95e0e2
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-06 21:34:38 -0300

    fix: corrigindo problema com o id do user no auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -33,4 +33,18 @@ export const { auth, signIn, signOut } = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token?.id && session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
 });
