bc1f9b539187a372e388852eca72f88b99a7318b
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-12 21:59:43 -0300

    feat: implementando tabela de visualização de usuários e botão de cadastro de novo usuário na página de gestão de usuários

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,7 +1,26 @@
-export default function AuthorizationsPage() {
+import { getUsers } from "@/actions/get-users";
+import { UsersTable } from "@/components/users/users-table";
+import { OpenCreateUserDialogButton } from "@/components/users/open-create-user-dialog-button";
+
+export default async function UsersManagementPage() {
+  const users = await getUsers();
+
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Painel de Gestão de Usuários</h1>
+    <div className="px-6 pb-4">
+      <div className="space-y-6">
+        <div className="flex flex-row justify-between items-center">
+          <h1 className="text-3xl font-bold">Painel de Gestão de Usuários</h1>
+          <OpenCreateUserDialogButton />
+        </div>
+
+        {users.length === 0 ? (
+          <div className="p-10 text-center border rounded-lg bg-white">
+            <p className="text-muted-foreground">Nenhum usuário encontrado.</p>
+          </div>
+        ) : (
+          <UsersTable data={users} />
+        )}
+      </div>
     </div>
   );
 }
