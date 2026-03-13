1ac32ac990684fb78a8f2718e1b2ea129f00e8da
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-12 21:23:49 -0300

    feat: criando action básica para get de users do banco

---
+++ src/actions/get-users.ts
@@ -0,0 +1,28 @@
+"use server";
+import prisma from "@/lib/prisma";
+
+// -------------------------- TODO -------------------------- //
+// Adicionar paginação com número de itens por página e número da página
+// Adicionar variável para que o usuário escolha a ordenação (asc ou desc)
+// Adicionar filtro por nome ou email
+export const getUsers = async () => {
+  try {
+    const users = await prisma.user.findMany({
+      include: {
+        authorizations: {
+          select: {
+            id: true, // Apenas para mostrar o número de autorizações, não precisa de mais detalhes aqui
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return users || [];
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    return [];
+  }
+};
