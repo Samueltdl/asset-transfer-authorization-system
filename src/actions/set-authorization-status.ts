f5f523768c7cd518c76948065d88b825406c545b
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-17 18:46:13 -0300

    feat: criando arquivo para as actions de set de status das authorizations

---
+++ src/actions/set-authorization-status.ts
@@ -0,0 +1,65 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth } from "@/auth";
+import { revalidatePath } from "next/cache";
+
+async function validUser(id: number) {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    return { error: "Não autorizado. Faça login novamente." };
+  }
+
+  try {
+    const authorization = await prisma.authorization.findUnique({
+      where: { id },
+      select: { userId: true, authorizationStatus: true },
+    });
+
+    if (!authorization) {
+      return { error: "Autorização não encontrada." };
+    }
+
+    // Verificar se o usuário logado é o proprietário da autorização ou um admin
+    if (
+      authorization.userId !== Number(session.user.id) &&
+      session.user.role !== "ADMIN"
+    ) {
+      return { error: "Não autorizado." };
+    }
+
+    return { success: true, authorization: authorization };
+  } catch (error) {
+    console.error("Erro ao validar usuário:", error);
+    return { error: "Falha ao validar usuário." };
+  }
+}
+
+// Função para registrar a aprovação de uma autorização, atualizando seu status
+export async function setAproved(id: number) {
+  // Verificar se o usuário é válido e tem permissão para registrar a aprovação
+  const validation = await validUser(id);
+  if (validation?.error) {
+    return validation.error; // Retorna o erro se a validação falhar
+  }
+
+  try {
+    // Registra a aprovação apenas se o status atual for "PENDING", para evitar requisições desnecessárias ou mudanças de status indevidas
+    if (validation.authorization?.authorizationStatus == "PENDING") {
+      // Registra a aprovação atualizando o status
+      await prisma.authorization.update({
+        where: { id },
+        data: {
+          authorizationStatus: "APPROVED",
+        },
+      });
+    }
+
+    revalidatePath("/dashboard/authorizations");
+    return { success: true };
+  } catch (error) {
+    console.error("Erro ao registrar aprovação:", error);
+    return { error: "Falha ao registrar a aprovação no sistema." };
+  }
+}
