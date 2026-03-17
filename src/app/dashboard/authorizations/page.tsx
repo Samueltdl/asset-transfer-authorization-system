ccd34e37c6cdc29dcf0fa92572b7da5765edba86
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-17 15:21:41 -0300

    refactor: implementando tabela e cadastro de novas autorizações na página de autorizações

--- src/app/dashboard/authorizations/page.tsx
+++ src/app/dashboard/authorizations/page.tsx
@@ -1,7 +1,34 @@
-export default function AuthorizationsPage() {
+import { getAuthorizations } from "@/actions/get-authorizations";
+import { AuthorizationsTable } from "@/components/authorizations/authorizations-table";
+import { OpenCreateAuthorizationDialogButton } from "@/components/authorizations/open-create-authorization-dialog-button";
+import { auth } from "@/auth";
+
+export default async function AuthorizationsPage() {
+  const session = await auth();
+  const authorizations = await getAuthorizations();
+
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Visualização de Autorizações</h1>
+    <div className="px-6 pb-4">
+      <div className="space-y-6">
+        <div className="flex flex-row justify-between items-center">
+          <h1 className="text-3xl font-bold">Autorizações de Saída</h1>
+          <OpenCreateAuthorizationDialogButton />
+        </div>
+
+        {authorizations.length === 0 ? (
+          <div className="p-10 text-center border rounded-lg bg-white">
+            <p className="text-muted-foreground">
+              Nenhuma autorização encontrada.
+            </p>
+          </div>
+        ) : (
+          <AuthorizationsTable
+            data={authorizations}
+            currentUserId={Number(session?.user?.id)}
+            currentUserRole={session?.user?.role as string}
+          />
+        )}
+      </div>
     </div>
   );
 }
