dd625542ce9c527d676a05497434d625f72f4d4d
Samuel Lemos <samueltdelemos@gmail.com>
2026-03-17 15:25:07 -0300

    refactor: finalizando a refatoração com a implementação dos cards de resumo no dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,16 @@
-import { getAuthorizations } from "@/actions/get-authorizations";
-import { AuthorizationsTable } from "@/components/authorizations/authorizations-table";
-import { OpenCreateAuthorizationDialogButton } from "@/components/authorizations/open-create-authorization-dialog-button";
-import { auth } from "@/auth";
+import { SummaryCards } from "@/components/dashboard/summary-cards";
 
-export default async function DashboardPage() {
-  const session = await auth();
-  const authorizations = await getAuthorizations();
-
+export default function DashboardPage() {
   return (
-    <div className="px-6 pb-4">
-      <div className="space-y-6">
-        <div className="flex flex-row justify-between items-center">
-          <h1 className="text-3xl font-bold">Autorizações de Saída</h1>
-          <OpenCreateAuthorizationDialogButton />
-        </div>
-
-        {authorizations.length === 0 ? (
-          <div className="p-10 text-center border rounded-lg bg-white">
-            <p className="text-muted-foreground">
-              Nenhuma autorização encontrada.
-            </p>
-          </div>
-        ) : (
-          <AuthorizationsTable
-            data={authorizations}
-            currentUserId={Number(session?.user?.id)}
-            currentUserRole={session?.user?.role as string}
-          />
-        )}
+    <div className="flex flex-col space-y-6 px-6 pb-4">
+      <div>
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+        <p className="text-muted-foreground">
+          Visão geral do controle de patrimônios e autorizações.
+        </p>
       </div>
+
+      <SummaryCards />
     </div>
   );
 }
